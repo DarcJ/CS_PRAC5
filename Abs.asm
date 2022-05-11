4e73e406ef808ec3c475759cbaa8942eee714cca
DarcyWJ <64890014+DarcyWJ@users.noreply.github.com>
2022-05-11 16:28:56 +0930

    labels

--- Abs.asm
+++ Abs.asm
@@ -1,13 +1,19 @@
-@R1
-D=M //Store R1 in D
-@9
-D;JGL //D < 0 got to 9
-@R0
-M=D //Return D
-@11
-0;JMP //End Program
-@R0
-M=-D
-@11
-0;JMP
+    @R1
+    D=M //Store R1 in D
+
+    @NEGATIVE
+    D;JGL //D < 0 got to 9
+
+    @R0
+    M=D //Return D
+    @End
+    0;JMP //End Program
+
+(NEGATIVE)
+    @R0
+    M=-D
+    
+(END)
+    @END
+    0;JMP
 
