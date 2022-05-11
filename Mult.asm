c4c521a581c179940df3d56790faac603804c2ea
DarcyWJ <64890014+DarcyWJ@users.noreply.github.com>
2022-05-11 19:37:49 +0930

    f

--- Mult.asm
+++ Mult.asm
@@ -6,4 +6,23 @@
 // Multiplies R1 and R2 and stores the result in R0.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+//Get First number
+//Add to itself
+//Reduce second number by 1
+//Check if second number is equal to zero
+//If second number equal to zero --> End program 
+//Else keep looping.
+
+@R1
+D=M
+@R2
+A=M
+(LOOP)
+    M=M+D
+    A=A-1
+    @END
+    A;JLE
+    @LOOP
+(END)
+    @END
+    0;JMP
