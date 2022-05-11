519a393c802a46edf187768e69a32dccbb7fac23
DarcyWJ <64890014+DarcyWJ@users.noreply.github.com>
2022-05-11 16:16:43 +0930

    abs

--- Abs.asm
+++ Abs.asm
@@ -1,4 +1,15 @@
 // Calculates the absolute value of R1 and stores the result in R0.
 // (R0, R1 refer to RAM[0], and RAM[1], respectively.)
+@R1
+D=M
+@9
+D;JGL
+@R0
+M=D
+@11
+0;JMP
+@R0
+M = -D
+@11
+0;JMP
 
-// Put your code here.
