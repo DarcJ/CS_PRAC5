768026a68f65d3c3f5d510d4b97917727d5d01ea
DarcyWJ <64890014+DarcyWJ@users.noreply.github.com>
2022-05-11 16:40:55 +0930

    testing scripts

--- Abs.asm
+++ Abs.asm
@@ -2,17 +2,17 @@
     D=M //Store R1 in D
 
     @NEGATIVE
-    D;JGL //D < 0 got to 9
+    D;JLT //D < 0 got to 9
 
     @R0
     M=D //Return D
-    @End
+    @END
     0;JMP //End Program
 
 (NEGATIVE)
     @R0
     M=-D
-    
+
 (END)
     @END
     0;JMP
