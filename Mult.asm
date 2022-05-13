de5f24a4fe1c7082b3fd2f48c266882c69aa375a
DarcyWJ <64890014+DarcyWJ@users.noreply.github.com>
2022-05-13 19:33:57 +0930

    mult update for large pos

--- Mult.asm
+++ Mult.asm
@@ -1,72 +1,94 @@
-@Indicator
-M=0
+//Either R1 or R2 are zero, Jump to Zero Loop
+@R1
+D=M
+@ZEROLOOP
+D;JEQ
+@R2
+D=M
+@ZEROLOOP
+D;JEQ
+//If R1 is negative, jump to negLoop
 @R1
 D=M
 @NEGLOOP
 D;JLT
+//R2 is negative & R1 is positive
 @R2
 D=M
 @NEGATE
 D;JLT
-@R1
-D=M
-@ZEROLOOP
-D;JEQ
-@R2
-D=M
-@ZEROLOOP
-D;JEQ
+
+//Both are positive
 @LOOP
 0;JMP
+
 (LOOP)
-    @R1
-    D=M
     @R0
-    M=M+D
+    D=M
     @R2
+    D=D+M
+    @R0
+    M=D
+    @R1
     D=M-1
     M=D
+
+    @R1
+    D=M
     @SIGNCHECK
     D;JEQ
+    
     @LOOP
     0;JMP
  (NEGLOOP)
     @Indicator
     M=1
+
     @R2
     D=M
+
     @DOUBLENEG
     D;JLT
     @0
     D=0
+
     @R1
     D=D-M
     M=D
+
     @LOOP
     0;JMP
 (NEGATE) //Change sign of R2 but set status to 1 to indicate a necessary sign change back to neg
     @Indicator
     M=1
+
     @0
     D=0
+
     @R2
     D=D-M
     M=D
+
     @LOOP
     0;JMP
 (DOUBLENEG)
     @Indicator
     M=0
+
     @0
     D=0
-    @R2
+
+    @R1
     D=D-M  //Converting to R2 positive
     M=D
+
     @0
     D=0
-    @R1 //Converting R1 Pos
+
+    @R2 //Converting R1 Pos
     D=D-M
     M=D
+
     @LOOP
     0;JMP
 (ZEROLOOP)
